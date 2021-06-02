1771ccfff0f7af7c2f0d418d591f3116632dbe3f
niehaitao <nie.haitao@gmail.com>
2021-06-02 11:45:10 +0200

    feat(lil-003): cicd tools - jenkins config as code

---
+++ sre-simply-re-explained/cicd/data/sleep-pipeline.groovy
@@ -0,0 +1,21 @@
+#!/usr/bin/groovy
+GString POD_LABEL = "your-job-${UUID.randomUUID().toString()}"
+
+pipeline {
+    agent { kubernetes { label POD_LABEL } }
+    parameters {
+        choice(name: 'EXPECT_STATUS', choices: ['SUCCESS', 'UNSTABLE', 'FAILURE', 'ABORTED', 'CYCLE'])
+        choice(name: 'EXPECT_SLEEP',  choices: ['1', '10', '20', '30', '60'])
+    }
+    stages {
+        stage('Hello') {
+            steps { script { echo "Hello World ${params.EXPECT_STATUS} !" } }
+        }
+        stage('Sleep'){
+            steps { sh "sleep ${params.EXPECT_SLEEP}" }
+        }
+        stage('Status'){
+            steps { script { currentBuild.result = params.EXPECT_STATUS } }
+        }
+    }
+}
